7b21794a75cc057e287b2d39139e651071e6b95a
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-03-13 06:19:07 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,15 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// import { ToastContainer } from 'react-toastify';
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(reg => console.log("Service Worker registered", reg))
+      .catch(err => console.log("Service Worker registration failed:", err));
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
