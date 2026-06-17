13526a2026259dd223a2c39235ac14883366c313
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-06-17 06:57:34 +0200

    Adjust top position of NotificationBell component

--- src/components/NotificationBell.jsx
+++ src/components/NotificationBell.jsx
@@ -573,7 +573,7 @@ const css = `
   transition: all 0.2s cubic-bezier(0.4, 0, 0.2, 1);
   flex-shrink: 0;
   left:50px;
-  top:12px;
+  top:22px;
 }
 .fb-btn:hover {
   background: ${THEME.bgHover};
